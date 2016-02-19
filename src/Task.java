85cbf8d5691484d281efadaaded2d9d793a4cc63
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-02-20 01:58:09 +0800

    First Commit

---
+++ src/Task.java
@@ -0,0 +1,16 @@
+
+public class Task {
+	
+	private String description;
+	private long startDate, endDate, dueDate;
+	private long startTime, endTime, dueTime;
+	
+	public Task()  {
+		
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+
+}
